66446703766bd942da4178bc1cd01189fc60e055
AlteredCow <austint319@gmail.com>
2017-05-23 18:53:32 -0400

    Final major updates

--- Aloha/irrelvant/Aloha JavaScript.js
+++
@@ -1,2 +0,0 @@
-// JavaScript Document
-<a href="#" onclick="document.getElementById('the_div').style.display=(document.getElementById('the_div').style.display=='block')?'none':'block';">Show/Hide Div</a>
--- Aloha/irrelvant/AsianMenuScript.js
+++
@@ -1,8 +0,0 @@
-// Collapsable table contents.
-
-function show(ID)
-{
-var element = document.getElementById(ID);
-if(element.style.display == 'block') {element.style.display = 'none';}
-else {element.style.display = 'block';}
-} 
